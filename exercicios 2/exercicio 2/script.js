d7f2b3401984364a610b5bd2c404171e5e90c33c
leandro pereira <lp2694941@gmail.com>
2026-04-22 21:44:25 -0400

    Add files via upload

---
+++ exercicios 2/exercicio 2/script.js
@@ -0,0 +1,5 @@
+let anoNascimento = prompt ("digite o ano do seu nascimento")
+let anoAtual = prompt ("digite o ano atual: ")
+anoNascimento = Number(anoNascimento);
+anoAtual = Number(anoAtual);
+let idade = anoNascimento - anoAtual;
